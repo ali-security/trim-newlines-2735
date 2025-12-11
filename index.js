112c27c17b615a387038d0b91f8d044adbcfa0bf
Seal Security <devops-admin@sealsecurity.io>
2025-12-11 15:09:01 +0000

    patched v3.0.0

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 'use strict';
-module.exports = string => string.replace(/^[\r\n]+/, '').replace(/[\r\n]+$/, '');
-module.exports.start = string => string.replace(/^[\r\n]+/, '');
-module.exports.end = string => string.replace(/[\r\n]+$/, '');
+
+module.exports = string => {
+	const trimmed = module.exports.start(string);
+	return module.exports.end(trimmed);
+};
+
+module.exports.start = string => {
+	let start = 0;
+	const end = string.length;
+
+	while (start < end && (string[start] === '\r' || string[start] === '\n')) {
+		start++;
+	}
+
+	return start > 0 ? string.slice(start, end) : string;
+};
+
+module.exports.end = string => {
+	let end = string.length;
+
+	while (end > 0 && (string[end - 1] === '\r' || string[end - 1] === '\n')) {
+		end--;
+	}
+
+	return end < string.length ? string.slice(0, end) : string;
+};
